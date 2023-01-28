a58f03571ff6b741252b902037d8fbd083411b13
unknown <danielhh.dev@gmail.com>
2023-01-28 16:35:56 -0600

    Navbar estilizada

--- src/components/navbar/MainNavBar.jsx
+++ src/components/navbar/MainNavBar.jsx
@@ -5,22 +5,26 @@ import FlechaDerecha from "../icons/FlechaAbajo";
 import MenuIcon from "../icons/MenuIcon";
 import NavSearchIcon from "../icons/NavSearchIcon";
 
-
 import CartModal from "../CartModal/CartModal";
 import LogoIcon from "../icons/LogoIcon";
 
 const NavBar = () => {
+  const [navClass, setNavClass] = useState(
+    "hidden p-8 text-white md:flex md:flex-row md:h-auto md:mr-auto md:gap-4 md:static md:p-0"
+  );
 
-const [navClass,setNavClass] = useState('hidden p-8 text-white md:flex md:flex-row md:h-auto md:mr-auto md:gap-4 md:static md:p-0')
+  const handleOpenMenu = () => {
+    setNavClass(
+      "absolute z-10 text-white top-0 p-2 left-0 h-[105.6vh] md:h-auto md:p-8 bg-rosa-oscuro  flex gap-y-5 font-bold md:font-normal w-3/5 md:w-[50%] flex-col md:flex-row  md:gap-4 md:static md:p-0"
+    );
+  };
 
-const handleOpenMenu = () => {
-    setNavClass('absolute z-10 text-white top-0 p-2 left-0 h-[105.6vh] md:h-auto md:p-8 bg-rosa-oscuro  flex gap-y-5 font-bold md:font-normal w-3/5 md:w-[50%] flex-col md:flex-row  md:gap-4 md:static md:p-0')
-}
-
-const handleCloseMenu = () => {
-    setNavClass('hidden p-8 font-bold md:flex md:flex-row md:h-auto md:mr-auto md:gap-4 md:static md:p-0')
-}
-    /* const [isOpenMenu, setIsOpenMenu] = useState(false)
+  const handleCloseMenu = () => {
+    setNavClass(
+      "hidden p-8 font-bold md:flex md:flex-row md:h-auto md:mr-auto md:gap-4 md:static md:p-0"
+    );
+  };
+  /* const [isOpenMenu, setIsOpenMenu] = useState(false)
 
     const handleOpenMenu = () => { 
         setIsOpenMenu(true)
@@ -30,61 +34,68 @@ const handleCloseMenu = () => {
         setIsOpenMenu(false)
     } */
 
-    const [dropMenu, setDropMenu] = useState('absolute hidden bg-rosa-oscuro ')
+  const [dropMenu, setDropMenu] = useState("absolute hidden bg-rosa-oscuro ");
 
-    const [click,setClick] = useState(false)
+  const [click, setClick] = useState(false);
 
-    const handleClickDropMenu = ()=> {
-        setClick(!click)
-        click
-            ?setDropMenu(' flex flex-col md:absolute  bg-rosa-oscuro pl-4 md:justify-start md:pl-0 ' )
-            :setDropMenu('hidden')
-    }
+  const handleClickDropMenu = () => {
+    setClick(!click);
+    click
+      ? setDropMenu(
+          " flex flex-col md:absolute  bg-rosa-oscuro pl-4 md:justify-start md:pl-0 "
+        )
+      : setDropMenu("hidden");
+  };
 
-    const [isOpenModal, setIsOpenModal] = useState(false)
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-    return(
-        <header className=" relative flex bg-rosa-oscuro p-5 items-center justify-between ">
-            {/* podria poner un selecSelect */}
-            <button className="md:hidden mr-auto" onClick={handleOpenMenu}><MenuIcon/></button>
-            <nav className={navClass}> 
-                <button onClick={handleCloseMenu} className="mb-5 md:hidden"><CloseIcon/></button>
-                <a href="/">Inicio</a>
-                <div onClick={handleClickDropMenu} className='cursor-pointer z-10 '> 
-                    Productos
-                    <ul className={dropMenu}>
-                        <li className="py-2  pl-1 pr-2">Tazas/Botellas</li>
-                        <li className="py-2 pl-1 pr-2">Kits</li>
-                        <li className="py-2 pl-1 pr-2">Cotillones</li>
-                        <li className="py-2 pl-1 pr-2">Indumentaria</li>
-                        <li className="py-2 pl-1 pr-2">Papeleria</li>
-                        <li className="py-2 pl-1 pr-2">Otros</li>
-                    </ul>
-                </div>
-                <a href="/">Contacto</a>
-                <a href="/">Sobre nosotros</a>
-            </nav>
-            <div className=" mr-auto "><LogoIcon/></div>
-            
-            <div className=" flex gap-4 mx-2">
-                <div className="hidden md:block">
+  return (
+    <header className=" relative flex items-center justify-between bg-rosa-oscuro p-5 ">
+      {/* podria poner un selecSelect */}
+      <button className="mr-auto md:hidden" onClick={handleOpenMenu}>
+        <MenuIcon />
+      </button>
+      <nav className={navClass}>
+        <button onClick={handleCloseMenu} className="mb-5 md:hidden">
+          <CloseIcon />
+        </button>
+        <a href="/">Inicio</a>
+        <div onClick={handleClickDropMenu} className="z-10 cursor-pointer ">
+          Productos
+          <ul className={dropMenu}>
+            <li className="py-2  pl-1 pr-2">Tazas/Botellas</li>
+            <li className="py-2 pl-1 pr-2">Kits</li>
+            <li className="py-2 pl-1 pr-2">Cotillones</li>
+            <li className="py-2 pl-1 pr-2">Indumentaria</li>
+            <li className="py-2 pl-1 pr-2">Papeleria</li>
+            <li className="py-2 pl-1 pr-2">Otros</li>
+          </ul>
+        </div>
+        <a href="/">Contacto</a>
+        <a href="/">Sobre nosotros</a>
+      </nav>
+      <div className=" mr-auto ">
+        <LogoIcon />
+      </div>
 
-                <NavSearchIcon />
-                </div>
-                <button onClick={()=>setIsOpenModal(!isOpenModal)}><CartIcon/></button>
-                {isOpenModal && <CartModal/>}
-            </div>
-
-        </header>
-    )
-}
+      <div className=" mx-2 flex gap-4">
+        <div className="hidden md:block">
+          <NavSearchIcon />
+        </div>
+        <button onClick={() => setIsOpenModal(!isOpenModal)}>
+          <CartIcon />
+        </button>
+        {isOpenModal && <CartModal />}
+      </div>
+    </header>
+  );
+};
 
 export default NavBar;
 
 /* Supongamos que necesito los valores que tailwind no me da. Entonces, podria hacer p-y-[*valor*] , pero no es lo ideal, deberia hacer algo generico porque sino deberia estar atento a cada componente */
 
 /* Deberia tratar de minificar todo, creo que vite lo hace */
-
 
 /* 
 
