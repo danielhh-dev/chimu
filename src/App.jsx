42465de88240c8957b29f7bc6431b00a16f2aca6
unknown <danielhh.dev@gmail.com>
2023-01-29 08:10:18 -0600

    Router agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UseCartContext from "./components/Context/UseCartContext";
 import Footer from "./components/Footer/Footer";
 import MainProduct from "./components/ItemDetail/mainProduct";
 import NavBar from "./components/navbar/MainNavBar";
-
-import UseCartContext from "./components/Context/UseCartContext";
 import CartPage from "./components/CartPage/CartPage";
 import SearchIcon from "./components/icons/SearchIcon";
 import AboutPage from "./components/AboutPage/AboutPage";
@@ -10,17 +10,15 @@ import ContactPage from "./components/ContactPage/ContactPage";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <UseCartContext>
         <NavBar />
-        <ContactPage />
-        {/* <AboutPage/> */}
-        {/* <section className=" md:hidden grid grid-row-1 justify-center"><SearchIcon /></section>
-            <MainProduct/> */}
-        {/* <CartPage/> */}
+        <Routes>
+          <Route path="/" element={<ContactPage />} />
+        </Routes>
         <Footer />
       </UseCartContext>
-    </>
+    </BrowserRouter>
   );
 };
 
