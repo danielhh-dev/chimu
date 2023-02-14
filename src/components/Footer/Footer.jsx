51b03d15f6da31867f7b05744c276e96e725a8c9
unknown <danielhh.dev@gmail.com>
2023-02-14 00:16:53 -0600

    footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,21 +8,28 @@ const Footer = () => {
   return (
     <footer className="  mt-[180px] flex h-44 w-full flex-row items-end  justify-between bg-color-verde pl-4 pt-7 pr-5 pb-8 md:pb-5">
       <nav className="flex flex-col gap-3 ">
-        <NavLink to="/" className="text-sm text-white md:text-lg ">
+        <NavLink className="text-sm text-white md:text-lg " to="/">
           Inicio
         </NavLink>
-        <NavLink to="/contact" className="text-sm text-white md:text-lg ">
+        <NavLink className="text-sm text-white  md:text-lg" to="/category">
+          Productos{" "}
+        </NavLink>
+        <NavLink className="text-sm text-white  md:text-lg" to="/contact">
           Contacto
         </NavLink>
-        <NavLink to="/about" className="text-sm text-white md:text-lg ">
-          Sobre nosotros
+        <NavLink className="text-sm text-white  md:text-lg" to="/about">
+          Sobre mi
         </NavLink>
       </nav>
 
       <div>
         <div className="flex w-16 flex-wrap gap-2.5 md:w-full ">
-          <img src={gmailIcon} alt="" />
-          <img src={whatsAppIcon} alt="" />
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=someone@example.com&su=SUBJECT&body=BODY&bcc=someone.else@example.com">
+            <img src={gmailIcon} alt="" />
+          </a>
+          <a href="https://api.whatsapp.com/send/?phone=5492302554639&text=Hola">
+            <img src={whatsAppIcon} alt="" />
+          </a>
           <img src={instagramIcon} alt="" />
           <img src={facebookIcon} alt="" />
         </div>
