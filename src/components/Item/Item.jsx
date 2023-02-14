9b98f74ec1f433e3ca8e2390da9eefbdcbe7c61e
unknown <danielhh.dev@gmail.com>
2023-02-14 00:07:37 -0600

    item actualizado

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,10 +2,12 @@ import { NavLink } from "react-router-dom";
 
 const Item = ({ image, name, price, id }) => {
   return (
-    <NavLink to={`/detail/${id}`} >
-      <img src={image} alt="" className="w-40" />
-      <p className="card-text">{name}</p>
-      <p className="card-text">ARS {price}</p>
+    <NavLink to={`/detail/${id}`}>
+      <img src={image} alt="" className="h-[220px] w-[220px]" />
+      <div className="text-center">
+        <p className="card-text">{name}</p>
+        <p className="card-text">{price}ARS</p>
+      </div>
     </NavLink>
   );
 };
