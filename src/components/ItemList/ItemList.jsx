fb0593746c100459cb437dbe458ee5735b013f10
unknown <danielhh.dev@gmail.com>
2023-02-14 00:06:56 -0600

    itemList actualizado

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,9 +2,9 @@ import Item from "../Item/Item";
 
 const ItemList = ({ products }) => {
   return (
-    <div className=" flex flex-wrap gap-8 items-center justify-between">
+    <div className=" m-auto  flex w-9/12 flex-wrap justify-center  gap-6">
       {products.map((product) => (
-        <div  key={product.id}>
+        <div key={product.id}>
           <Item {...product} />
         </div>
       ))}
